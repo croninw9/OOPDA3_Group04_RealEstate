40f039bfb3e49cb2140d2a3a05f99a06277e0538
chris1estrada <36424599+chris1estrada@users.noreply.github.com>
2018-04-17 22:19:27 -0400

    Update Commerial.java

--- Commerial.java
+++ Commerial.java
@@ -3,12 +3,18 @@
  * @author Jared Evangelista
  *
  */
-public class Commerial extends Building
-{
-	String Company;
+public class Commerial extends Building{
+
+	String company;
 	
-	public String Company()
+	public Commerial(String type, int price, int squareFt, int floor, int room, int bathroom, String location, String fileName, String company)
 	{
-		return Company;
+		super(price, squareFt, floor, room, location, fileName);
+		this.company = company;
+	}
+	
+	public String getCompany()
+	{
+		return company;
 	}
 }
