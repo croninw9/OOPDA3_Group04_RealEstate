98e25df534bc4f1f66d221ddc21b6e0d408839e5
duongt3 <36242637+duongt3@users.noreply.github.com>
2018-04-17 21:52:16 -0400

    Add files via upload

--- Appraise.java
+++ Appraise.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
@@ -17,6 +18,7 @@ public class Appraise{
 	private JButton appraise;
 	private JButton list;
 	private JPanel tPanel;
+	private JPanel t2Panel;
 	private JPanel bPanel;
 	private BuildingReader br;
 	private double price;
@@ -32,7 +34,8 @@ public class Appraise{
 
 	private ArrayList<Residential> buildings;
 	
-	public Appraise() {
+	public Appraise() 
+	{
 		br = new BuildingReader();
 		buildings = br.getBuildings("buildings.csv");
 		openImage = new OpenImage();
@@ -51,6 +54,7 @@ public class Appraise{
 		location = new JTextField(30);
 		
 		tPanel = new JPanel(new BorderLayout());
+		t2Panel = new JPanel();
 		JPanel north = new JPanel(new BorderLayout());
 		JPanel center = new JPanel(new BorderLayout());
 		JPanel houseP = new JPanel();
@@ -59,18 +63,18 @@ public class Appraise{
 		JPanel bathroomP = new JPanel();
 		JPanel floorsP = new JPanel();
 		JPanel locationP = new JPanel();
-		
-		JLabel label1 = new JLabel("Enter House Type: "); 
+	
+		JLabel label1 = new JLabel("Enter House Type:               "); 
 		label1.setFont(new Font("Century", Font.BOLD, 20)); 
 		label1.setForeground(new Color(102, 205, 170)); 
 		houseP.add(label1); 
 		
-		JLabel label2 = new JLabel("Enter amount of rooms: "); 
+		JLabel label2 = new JLabel("Enter amount of rooms:        "); 
 		label2.setFont(new Font("Century", Font.BOLD, 20)); 
 		label2.setForeground(new Color(102, 205, 170)); 
 		roomP.add(label2); 
 		
-		JLabel label3 = new JLabel("Enter amount of square feet: "); 
+		JLabel label3 = new JLabel("Enter amount of square feet:"); 
 		label3.setFont(new Font("Century", Font.BOLD, 20)); 
 		label3.setForeground(new Color(102, 205, 170)); 
 		squareFtP.add(label3); 
@@ -80,12 +84,12 @@ public class Appraise{
 		label4.setForeground(new Color(102, 205, 170)); 
 		bathroomP.add(label4); 
 		
-		JLabel label5 = new JLabel("Enter amount of Floors: "); 
+		JLabel label5 = new JLabel("Enter amount of Floors:       "); 
 		label5.setFont(new Font("Century", Font.BOLD, 20)); 
 		label5.setForeground(new Color(102, 205, 170));
 		floorsP.add(label5);
 		
-		JLabel label6 = new JLabel("Enter location :");
+		JLabel label6 = new JLabel("Enter location:                    ");
 		label6.setFont(new Font("Century", Font.BOLD, 20));
 		label6.setForeground(new Color(102, 205, 170));
 		locationP.add(label6);
@@ -118,14 +122,17 @@ public class Appraise{
 		bathroomP.setBackground(new Color(47, 79, 79));
 		floorsP.setBackground(new Color(47, 79, 79));
 		locationP.setBackground(new Color(47, 79, 79));
+
 		tPanel.add(north, BorderLayout.NORTH);
-		tPanel.add(center, BorderLayout.SOUTH);
+		tPanel.add(center, BorderLayout.CENTER);
+		t2Panel.setBackground(new Color(47, 79, 79));
+		t2Panel.add(tPanel);
 	}
 	
 	public void setUpButtonPanel() {
 		bPanel = new JPanel();
 		appraise = new JButton("Appraise");
-		appraise.addActionListener(e -> appraiseHouse());
+		appraise.addActionListener(e -> checkAppraise());
 		list = new JButton("List");
 		list.addActionListener(e -> {
 			try {
@@ -134,12 +141,62 @@ public class Appraise{
 			}
 		});
 		
+		
+		JButton imageSelect = new JButton("Select Image");
+		imageSelect.addActionListener(e -> openImage.openImage());
+		
+		appraise.setBackground(new Color(47, 79, 79));
+        appraise.setForeground(new Color(102, 205, 170));
+        appraise.setFocusPainted(false);
+        appraise.setFont(new Font("Century", Font.BOLD, 18));
+        
+        list.setBackground(new Color(47, 79, 79));
+        list.setForeground(new Color(102, 205, 170));
+        list.setFocusPainted(false);
+        list.setFont(new Font("Century", Font.BOLD, 18));
+        
+        imageSelect.setBackground(new Color(47, 79, 79));
+        imageSelect.setForeground(new Color(102, 205, 170));
+        imageSelect.setFocusPainted(false);
+        imageSelect.setFont(new Font("Century", Font.BOLD, 18));
+        
+        bPanel.setBackground(new Color(47, 79, 79));
+		
 		bPanel.add(appraise);
 		bPanel.add(list);
+		bPanel.add(imageSelect);
 	}
 	
+	public void checkAppraise() {
+		String error = "";
+		if(houseType.getText().equals("")) {
+			error += "Please add a Type of House \n";
+		}
+		if(numRoom.getText().equals("")) {
+			error += "Please add Number of Rooms \n";
+		}
+		if(squareFt.getText().equals("")) {
+			error += "Please add Size of Resident \n";
+		}
+		if(bathroom.getText().equals("")) {
+			error += "Please add Size of Resident \n";
+		}
+		if(floors.getText().equals("")) {
+			error += "Please add Size of Resident \n";
+		}
+		if(location.getText().equals("")) {
+			error += "Please add Size of Resident \n";
+		}
+		
+		if(error.equals("")) {
+			appraiseHouse();
+		}
+		else {
+			JOptionPane.showMessageDialog(null, error, "Warning", JOptionPane.ERROR_MESSAGE);
+		}
+	}
 	public void appraiseHouse() {
-		 
+		
 		type = houseType.getText().trim();
 		room = Integer.parseInt(numRoom.getText());
 		squareft = Integer.parseInt(squareFt.getText());
@@ -147,7 +204,9 @@ public class Appraise{
 		floorsNum = Integer.parseInt(floors.getText());;
 		locationNum = location.getText().trim();
 		
+		DecimalFormat df = new DecimalFormat("#.00"); 
 		price = ((avgType() + avgRoom() + avgSize() + avgBath() + avgFloor() + avgLocation())/6);
+		JOptionPane.showMessageDialog(null, ("The value of the House is : $" + df.format(price)));
 		System.out.println(price);
 		System.out.println(type + room + squareft + bathroomNum + floorsNum + locationNum);
 		
@@ -156,6 +215,10 @@ public class Appraise{
 	public void listHouse() throws IOException {
 		if(price != 0) {
 			br.addBuilding("buildings.csv", new Residential(type, price, squareft, floorsNum, room, bathroomNum, locationNum, openImage.getFileName()));
+			JOptionPane.showMessageDialog(null, "Congratulations! You've successfully listed the Residence");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Appraise the House before Listing", "Warning", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 	
@@ -198,7 +261,7 @@ public class Appraise{
 	
 	public JPanel tPanelReturn()
 	{
-		return tPanel;
+		return t2Panel;
 	}
 	
 	public JPanel imagePanelReturn() {
