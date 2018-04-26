e59d2f4507c563a70efe4eaf56fefd642bee1749
chris1estrada <36424599+chris1estrada@users.noreply.github.com>
2018-04-26 14:25:54 -0400

    Update Agency.java

--- Agency.java
+++ Agency.java
@@ -3,6 +3,7 @@ import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 
 /**
+ * The Agency class takes the role of filtering for the search 
  * @author estradac3
  *
  */
@@ -15,6 +16,18 @@ public class Agency {
 		buildings = br.getBuildings("buildings.csv");
 	}
 	
+	/**
+	 * returns desired traits
+	 * @param type of int  
+	 * @param room of int 
+	 * @param sqFt of int
+	 * @param bathNum of int 
+	 * @param floorNum of int 
+	 * @param locNum of int 	
+	 * @param price of int
+	 * @return ArrayList<Residential> which holds the 
+	 * houses that still contain the filtered qualities
+	 */
 	public ArrayList<Residential> filter(int type, int room, int sqFt, int bathNum, int floorNum, int locNum, int price)
 	{
 		if(type != 0) {
@@ -78,42 +91,70 @@ public class Agency {
 		return buildings;
 	}
 	
+	/**
+	 * filters the arraylist to only contain desired type
+	 * @param type holds the number for type
+	 */
 	public void filterType(String type){
 		buildings = buildings.stream()
 						.filter(res -> res.getType().equalsIgnoreCase(type))
 						.collect(Collectors.toCollection(ArrayList::new));
 	}
 	
+	/**
+	 * filters the arraylist to only contain desired rooms
+	 * @param type holds the number for rooms
+	 */
 	public void filterRoom(int room){
 		buildings = buildings.stream()
 						.filter(res -> res.getRoom() == room)
 						.collect(Collectors.toCollection(ArrayList::new));
 	}
 	
+	/**
+	 * filters the arraylist to only contain desired sqFt
+	 * @param type holds the number for sqFt
+	 */
 	public void filterSize(int sqFt) {
 		buildings = buildings.stream()
 				.filter(res -> (sqFt - 100) <= res.getSquareFt() && res.getSquareFt() <= (sqFt + 100))
 				.collect(Collectors.toCollection(ArrayList::new));
 	}
 	
+	/**
+	 * filters the arraylist to only contain desired bath
+	 * @param type holds the number for bath
+	 */
 	public void filterBathroom(int bath) {
 		buildings = buildings.stream()
 				.filter(res -> res.getBathroom() == bath)
 				.collect(Collectors.toCollection(ArrayList::new));
 	}
 
+	/**
+	 * filters the arraylist to only contain desired floor
+	 * @param type holds the number for floor
+	 */
 	public void filterFloor(int floor) {
 		buildings = buildings.stream()
 				.filter(res -> res.getFloor() == floor)
 				.collect(Collectors.toCollection(ArrayList::new));
 	}
 	
+	/**
+	 * filters the arraylist to only contain desired location
+	 * @param type holds the number for location
+	 */
 	public void filterLocation(String Location) {
 		buildings = buildings.stream()
 				.filter(res -> res.getLocation().equalsIgnoreCase(Location))
 				.collect(Collectors.toCollection(ArrayList::new));
 	}
 	
+	/**
+	 * filters the arraylist to only contain desired price
+	 * @param type holds the number for price
+	 */
 	public void filterPrice(int price) {
 		buildings = buildings.stream()
 				.filter(res -> res.getPrice() == price)
