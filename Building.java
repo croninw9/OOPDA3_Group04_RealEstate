0721b42de45fd36739e1c2ba1721802d3c6e6588
chris1estrada <36424599+chris1estrada@users.noreply.github.com>
2018-04-26 14:36:44 -0400

    Update Building.java

--- Building.java
+++ Building.java
@@ -22,50 +22,98 @@ public abstract class Building {
 		this.fileName = fileName;
 	}
 
+	/**
+	 * returns filename
+	 * @return fileName
+	 */
 	public String getFileName() {
 		return fileName;
 	}
 
+	/**
+	 * sets fileName to fileName
+	 * @param fileName
+	 */
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
 
+	/**
+	 * returns floor
+	 * @return floor
+	 */
 	public int getFloor() {
 		return floor;
 	}
 
+	/**
+	 * sets a new floor
+	 * @param floor
+	 */
 	public void setFloor(int floor) {
 		this.floor = floor;
 	}
 
+	/**
+	 * returns price
+	 * @return price
+	 */
 	public double getPrice() {
 		return price;
 	}
 
+	/**
+	 * returns squareFt
+	 * @return squareFt
+	 */
 	public int getSquareFt() {
 		return squareFt;
 	}
 
+	/**
+	 * returns room
+	 * @return room
+	 */
 	public int getRoom() {
 		return room;
 	}
 
+	/**
+	 * return location
+	 * @return location
+	 */
 	public String getLocation() {
 		return location;
 	}
 
+	/**
+	 * set new price
+	 * @param price
+	 */
 	public void setPrice(double price) {
 		this.price = price;
 	}
 
+	/**
+	 * set new squareFt
+	 * @param squareFt
+	 */
 	public void setSquareFt(int squareFt) {
 		this.squareFt = squareFt;
 	}
 
+	/**
+	 * set new room
+	 * @param room
+	 */
 	public void setRoom(int room) {
 		this.room = room;
 	}
 
+	/**
+	 * set new location
+	 * @param location
+	 */
 	public void setLocation(String location) {
 		this.location = location;
 	}
